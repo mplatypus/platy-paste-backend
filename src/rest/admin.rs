c9e1dc726f1239607e2546a22ab47d341c8eb77b
mplaty <contact@mplaty.com>
2025-03-22 14:57:49 +1100

    Remove unused rest endpoints.

--- src/rest/admin.rs
+++
@@ -1,8 +0,0 @@
-use axum::{extract::DefaultBodyLimit, Router};
-
-use crate::app::app::App;
-
-pub fn generate_router() -> Router<App> {
-    Router::new()
-        .layer(DefaultBodyLimit::disable())
-}
