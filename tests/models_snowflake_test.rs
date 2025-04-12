e773ccb004359ef9050bcf3c45233050c9b881e0
mplaty <contact@mplaty.com>
2025-04-13 02:25:44 +1000

    Add snowflake tests.

---
+++ tests/models_snowflake_test.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+use platy_paste::models::snowflake::Snowflake;
+
+#[test]
+fn test_uniqueness() {
+    let snowflakes = vec![
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+        Snowflake::generate().expect("Failed to generate unique snowflake."),
+    ];
+
+    let set: HashSet<_> = snowflakes.iter().collect();
+
+    assert!(set.len() == snowflakes.len(), "Non-unique snowflake found: {snowflakes:?}");
+}
